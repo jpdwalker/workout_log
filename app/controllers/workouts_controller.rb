ed4f0409b0c3fe3582a7b4967e3a1336d924ef44
Joe Walker <jpdwalker@gmail.com>
2019-07-02 16:15:03 +0100

    created workout show and bringing through workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,5 @@
 class WorkoutsController < ApplicationController
+  before_action :find_workout, only: [:show, :edit, :update, :destroy]
 
   def index
   end
@@ -35,6 +36,7 @@ class WorkoutsController < ApplicationController
   end
 
   def find_workout
+    @workout = Workout.find(params[:id])
   end
 
 end
